486a9c3e4ebd164366b078bd82db18582469d484
Rajesh <r.rajesh@live.in>
2022-01-15 19:52:37 +0530

    After saving the customer in Group 15012020

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
 from django.contrib import messages
 from . models import *
 from . forms import OrderForm , CreateUserForm
@@ -16,9 +17,11 @@ def registerPage(request):
     if request.method == 'POST': 
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Account was created for ' + user)
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            group = Group.objects.get(name='customer')
+            user.groups.add(group)
+            messages.success(request, 'Account was created for ' + username)
             return redirect('login')
     context = {'form': form}
     return render(request, "accounts/register.html", context)
