fe279fb79742d3f8074ade6838a86280222db4b1
Rajesh <r.rajesh@live.in>
2022-01-21 21:55:50 +0530

    After Password Reset Email has been deployed 21012022

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -7,12 +8,14 @@ urlpatterns = [
     path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerPage, name="register"),
     path('user/', views.userPage, name="user-page"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset',),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done', ),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm',),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete',),
     path('account/', views.accountSettings, name="account"),
     path('products/', views.products, name="products"),
     path('customer/<str:pk_test>/', views.customer, name="customer"),
     path('create_order/<str:pk>/', views.createOrder, name="create_order"),
     path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
     path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
-
-
 ]
